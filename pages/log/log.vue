487bd533e2e6242de24a495436e07b46ec2eb261
xiaoguizi87 <xiaoguizi87@163.com>
2019-08-25 21:03:40 +0800

    增加收藏到我的小程序提示

--- pages/log/log.vue
+++ pages/log/log.vue
@@ -1,57 +1,55 @@
 <template>
-	<view class='container'>
-		<text class="title">学习记录</text>
-		<view v-for='(t, i) in logs' :key='i' class="row">
-			<text class="txt">{{t.logType}}</text>
-			<text class="txt">{{t.time}}</text>
-		</view>
-	</view>
+  <view class='container'>
+    <text class="title">学习记录</text>
+    <view v-for='(t, i) in logs' :key='i' class="row">
+      <text class="txt">{{t.logType}}</text>
+      <text class="txt">{{t.time}}</text>
+    </view>
+    <button class='shareBtn' open-type="share">分享</button>
+  </view>
 </template>
 
 <script>
-	const utils = require('../../utils/index.js')
+  const utils = require('../../utils/index.js')
 
-	export default {
-		data() {
-			return {
-				logs: []
-			}
-		},
-		methods: {
-			created() {
-			}
-		},
-		onLoad() {
-			utils.getStudyLog().then(res => {
-				console.log(res)
-				this.logs = res
-			})
-		},
-		onShareAppMessage: function(options) {
-			console.log('分享的代码！！')
-		}
-	}
+  export default {
+    data() {
+      return {
+        logs: []
+      }
+    },
+    methods: {
+      created() {}
+    },
+    onLoad() {
+      utils.getStudyLog().then(res => {
+        console.log(res)
+        this.logs = res
+      })
+    },
+    onShareAppMessage: function(options) {
+      console.log('分享的代码！！')
+    }
+  }
 </script>
 
 <style>
+  .title {
+    color: #1da049;
+    margin-bottom: 28rpx 0;
+    font-size: 20px;
+    text-align: center;
+  }
 
-	.title {
-		color: #1da049;
-		margin-bottom: 28rpx 0;
-		font-size: 20px;
-		text-align: center;
-	}
-	
-	.row {
-		width: 540rpx;
-		display: inline-flex;
-		justify-content: space-between;
-		margin: 20rpx;
-	}
-	
-	.txt {
-		font-size: 16px;
-		font-color: #34495E;
-	}
-	
+  .row {
+    width: 540rpx;
+    display: inline-flex;
+    justify-content: space-between;
+    margin: 20rpx;
+  }
+
+  .txt {
+    font-size: 16px;
+    font-color: #34495E;
+  }
 </style>
